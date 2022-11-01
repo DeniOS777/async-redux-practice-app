db7a983f1244527ad8ddde0ebc8fd17ebdc6bc21
DeniOS777 <Denios777@gmail.com>
2022-11-01 18:36:23 +0200

    deleted unused function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,7 @@
-import { duplicate } from './duplicate';
 import { Gallery } from './Gallery/Gallery';
 import { SelectGenre } from './SelectGenre/SelectGenre';
 
 export const App = () => {
-  console.log(duplicate([1, 1, 1, 2, 2, 5]));
   return (
     <div>
       <SelectGenre />
